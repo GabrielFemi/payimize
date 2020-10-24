51396d6430ad0e78172a91d0f66eca3752ed68a6
Gabriel Femi <gabrielfemi799@gmail.com>
2020-10-24 14:28:14 +0100

    Add style icon with go home button

--- resources/js/Pages/Register-As-Institution.vue
+++ resources/js/Pages/Register-As-Institution.vue
@@ -19,6 +19,19 @@
                 </a>
             </div>
         </div>
+        <div class="flex items-center justify-center">
+            <a class="btn btn-primary btn-lg inline-flex justify-center items-center w-full sm:w-auto mb-2 sm:mb-0" href="/register">
+                <svg class="mr-1 svg-icon" viewBox="0 0 20 20">
+                    <path fill="none" d="M18.271,9.212H3.615l4.184-4.184c0.306-0.306,0.306-0.801,0-1.107c-0.306-0.306-0.801-0.306-1.107,0
+                        L1.21,9.403C1.194,9.417,1.174,9.421,1.158,9.437c-0.181,0.181-0.242,0.425-0.209,0.66c0.005,0.038,0.012,0.071,0.022,0.109
+                        c0.028,0.098,0.075,0.188,0.142,0.271c0.021,0.026,0.021,0.061,0.045,0.085c0.015,0.016,0.034,0.02,0.05,0.033l5.484,5.483
+                        c0.306,0.307,0.801,0.307,1.107,0c0.306-0.305,0.306-0.801,0-1.105l-4.184-4.185h14.656c0.436,0,0.788-0.353,0.788-0.788
+                        S18.707,9.212,18.271,9.212z">
+                    </path>
+                </svg>
+                Go home
+            </a>
+        </div>
     </div>
 </template>
 
@@ -33,3 +46,20 @@ name: "Register-As-Institution",
 }
 </script>
 
+<style scoped>
+.svg-icon {
+    width: 1em;
+    height: 1em;
+}
+
+.svg-icon path,
+.svg-icon polygon,
+.svg-icon rect {
+    fill: #fff;
+}
+
+.svg-icon circle {
+    stroke: #fff;
+    stroke-width: 1;
+}
+</style>
