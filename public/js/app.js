aece71d390787e6afa111522f1d2abf98021fd01
Gabriel Femi <gabrielfemi799@gmail.com>
2020-10-24 20:42:37 +0100

    Check todo

--- public/js/app.js
+++ public/js/app.js
@@ -4359,7 +4359,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: "Index"
 });
--- resources/js/Pages/Transaction/SchoolFees/Index.vue
+++ resources/js/Pages/Transaction/SchoolFees/Index.vue
@@ -1,7 +1,6 @@
 <template>
     <div>
         <div class="alert bg-green-700 text-white mb-6" role="alert">All caught up!</div>
-        <!-- TODO: Use named route-->
         <inertia-link :href="route('dashboard')" class="p-2 rounded bg-blue-400"><- Dashboard</inertia-link>
     </div>
 
