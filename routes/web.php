e64eb740253879960d2ffcb07b70c8379aff673a
Gabriel Femi <gabrielfemi799@gmail.com>
2020-10-24 12:59:13 +0100

    Made home route for only guests

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::middleware('guest')->get('/', function () {
    return \Inertia\Inertia::render('Welcome');
 });
 
