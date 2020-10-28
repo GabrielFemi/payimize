b7baf578d9ecf6062bc63e4440642546e3daf75d
Gabriel Femi <gabrielfemi799@gmail.com>
2020-10-28 15:35:05 +0100

    Feat: onboard a new user

--- app/Actions/FinishOnboarding.php
+++ app/Actions/FinishOnboarding.php
@@ -5,12 +5,18 @@ namespace App\Actions;
 
 
 use App\Http\Requests\FinishOnboardingRequest;
-use Illuminate\Foundation\Auth\User;
+use App\Models\AcademicDetails;
+use App\Models\User;
+use App\Traits\CanBeOnboarded;
 
 class FinishOnboarding
 {
+    use CanBeOnboarded;
+
     public function __invoke(FinishOnboardingRequest $request)
     {
-        //
+        AcademicDetails::create(array_merge(['user_id' => auth()->id()], $request->validated()));
+        $this->onboard(auth()->id());
+        return redirect('/dashboard');
     }
 }
